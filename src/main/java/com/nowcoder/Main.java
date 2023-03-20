a53245220b5b3eb254f15d3fc3302a0f2ac917f0
chord <877865532@qq.com>
2023-03-20 23:18:14 +0800

    'Test1'

---
+++ src/main/java/com/nowcoder/Main.java
@@ -0,0 +1,11 @@
+package com.nowcoder;
+
+/**
+ * @author chord
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
